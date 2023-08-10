b7e4b369688b97904ad998a126ba80145ec683d8
Stuart Grange <s.k.grange@gmail.com>
2023-08-10 14:03:27 +0200

    :sparkles: enhance ra_to_df to handle multi later objects and add a date parser

--- R/ra_to_df.R
+++ R/ra_to_df.R
@@ -2,12 +2,18 @@
 #' 
 #' @param ra A \strong{terra} \code{SpatRaster} object.
 #' 
+#' @param variable_as_date If \code{ra}'s names represent dates, should the 
+#' dates be parsed and the variable renamed? 
+#' 
+#' @param tz If \code{variable_as_date} is \code{TRUE}, what time zone are the 
+#' dates stored in? 
+#' 
 #' @author Stuart K. Grange
 #' 
 #' @return Tibble.
 #' 
 #' @export
-ra_to_df <- function(ra) {
+ra_to_df <- function(ra, variable_as_date = FALSE, tz = "UTC") {
   
   # Check input
   stopifnot(inherits(ra, "SpatRaster"))
@@ -16,7 +22,7 @@ ra_to_df <- function(ra) {
   ra <- terra::as.points(ra)
   
   # To sf
-  # st_as_sf(ra)
+  # sf::st_as_sf(ra)
   
   # Bind geom and values together in a tibble and do some minor cleaning
   df <- dplyr::bind_cols(
@@ -28,6 +34,17 @@ ra_to_df <- function(ra) {
            hole = as.logical(hole)) %>% 
     relocate(hole,
              .before = x)
+  
+  # Make data longer
+  df <- tidyr::pivot_longer(df, -c(geom, part, hole, x, y), names_to = "variable")
+  
+  # Parse dates and rename variable
+  if (variable_as_date && stringr::str_detect(df$variable[1], "^X")) {
+    df <- df %>% 
+      rename(date = variable) %>% 
+      mutate(date = stringr::str_remove(date, "^X"), 
+             date = lubridate::ymd_hms(date, tz = tz, truncated = 3))
+  }
   
   return(df)
   
